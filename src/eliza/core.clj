53ad935f4737076e181c7428c3578939bb908f11
Michael Lu <lumichael94@gmail.com>
2015-07-31 12:58:02 -0400

    marytts is now integrated and working

--- src/eliza/core.clj
+++ src/eliza/core.clj
@@ -1,32 +1,49 @@
 (ns eliza.core
   (:gen-class)
   (:import [org.hypergraphdb HyperGraph])
-  (:import [marytts LocalMaryInterface]))
+  (:import [marytts LocalMaryInterface])
+  (:import [marytts.util.data.audio AudioPlayer]))
 
 (def location "./db")
-(def graph (new HyperGraph location))
-(def mary (new LocalMaryInterface))
 
-; Returns input
+(def graph (new HyperGraph location))
+(def mary-interface (new LocalMaryInterface))
+
+(defn mary-set-voice [lmi]
+  (.setVoice lmi (first (.getAvailableVoices lmi))))
+
+(defn play-audio [wav-snippet]
+  (let [player (new AudioPlayer wav-snippet)]
+    (do
+      (.start player)
+      (.join player))))
+
 (defn input-repl []
   (do
     (print "you: ")
     (flush)
     (read-line)))
+    ;(let [in (read-line)]
+    ;  (.get graph (.add graph "goodbye!")))))
 
-(defn output-repl [out]
-  (println (str "eliza: " out)))
+(defn output-repl [mary out]
+  (do
+  (println (str "eliza: " out))
+  (play-audio (.generateAudio mary out))))
 
-(defn repl []
+
+(defn repl [mary]
   (loop []
     (let [input (input-repl)]
     (when (not= input "exit")
-        (output-repl input)
+        (output-repl mary input)
         (recur)))))
+
 
 (defn -main 
   [& args] 
-    (repl))
-    ;(println (.get graph (.add graph "goodbye!")))
-    ;(.close graph)
-    ;))
+      (let [mary mary-interface]
+        (do 
+          (mary-set-voice mary)
+          (repl mary))))
+          ;(.close graph)))
