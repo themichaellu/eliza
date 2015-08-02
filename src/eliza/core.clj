a348f2466c3e9ad5d3cecf3f5417e842af673ba4
Michael Lu <lumichael94@gmail.com>
2015-08-01 20:22:46 -0400

    tested a basic traversal with 4 nodes.

--- src/eliza/core.clj
+++ src/eliza/core.clj
@@ -1,11 +1,11 @@
 (ns eliza.core
   (:gen-class)
-  (:import [org.hypergraphdb HyperGraph])
+  (:import [org.hypergraphdb HyperGraph HGHandle HGQuery$hg HGPlainLink])
+  (:import [org.hypergraphdb.algorithms DefaultALGenerator HGTraversal HGBreadthFirstTraversal])
   (:import [marytts LocalMaryInterface])
   (:import [marytts.util.data.audio AudioPlayer]))
 
 (def location "./db")
-
 (def graph (new HyperGraph location))
 (def mary-interface (new LocalMaryInterface))
 
@@ -28,22 +28,63 @@
 
 (defn output-repl [mary out]
   (do
-  (println (str "eliza: " out))
-  (play-audio (.generateAudio mary out))))
+  (print(str "eliza: " ))
+    (let [alGen (new DefaultALGenerator graph (HGQuery$hg/type HGPlainLink) (HGQuery$hg/type String) false true false)
+          trav (new HGBreadthFirstTraversal (.getHandle graph out) alGen)]
+      (print out)
+      (loop []
+        (when (= true (.hasNext trav))
+          (print (str (.get graph (.getSecond (.next trav)))))
+          (recur)))
+    ;(play-audio (.generateAudio mary item)))
+  (print "\n"))))
 
+
+(defn hello-world []
+  ;TODO: Not used at the moment
+  (HGQuery$hg/getAll graph (HGQuery$hg/type String)))
 
 (defn repl [mary]
   (loop []
     (let [input (input-repl)]
     (when (not= input "exit")
-        (output-repl mary input)
+        (output-repl mary "Hello")
         (recur)))))
 
+(defn add-items [vect]
+  "Adds items to graph and return a list of respective handles"
+  (map #(.add graph %) vect))
 
-(defn -main 
-  [& args] 
+(defn add-links [vect item1 item2]
+  (do
+  (.add graph (new HGPlainLink (into-array HGHandle [item1 item2])))
+  (let [index (.indexOf vect item2)]
+    ;If the index of the second element is less than size - 1
+    (if (< index (- (count vect) 1))
+      (add-links vect item2 (nth vect (+ index 1)))))))
+
+;TODO: Find a better way to do this
+(defn temp-add []
+  "Adds few strings to hypergraphdb"
+    (let [items ["Hello" "," " World" "!"]
+          vect (add-items items)]
+      (add-links vect (nth vect 0) (nth vect 1))
+      (doseq [link (HGQuery$hg/getAll graph (HGQuery$hg/type HGPlainLink))]
+      (println link))))
+
+(defn temp-remove []
+  "Removes a temp strings from hypergraphdb"
+  (let [remove-list (HGQuery$hg/findAll graph (HGQuery$hg/type String))]
+    (doseq [item remove-list]
+      (do
+        ;(println (.get graph item))
+        (.remove graph item)))))
+  
+(defn -main [& args] 
       (let [mary mary-interface]
         (do 
           (mary-set-voice mary)
-          (repl mary))))
-          ;(.close graph)))
+          (temp-add)
+          (repl mary)
+          (temp-remove)
+          (.close graph))))
