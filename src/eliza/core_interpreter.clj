d25b0615c88128fa6f0b19168a44d007d418714c
Michael Lu <lumichael94@gmail.com>
2015-08-04 10:20:14 -0400

    broken code, null pointer exception

--- src/eliza/core_interpreter.clj
+++ src/eliza/core_interpreter.clj
@@ -1,77 +1,33 @@
 (ns eliza.core_interpreter
   (:gen-class)
-  (:require [eliza.hypergraphdb :as hypergraph :refer [graph]])
-  (:require [eliza.marytts :as marytts :refer [mary-interface audio-player]])
-  (:import [org.hypergraphdb HGEnvironment HyperGraph HGHandle HGQuery$hg HGPlainLink])
-  (:import [org.hypergraphdb.algorithms DefaultALGenerator HGTraversal HGBreadthFirstTraversal]))
+  (:require [eliza.hypergraphdb :as hypergraph])
+  (:require [eliza.marytts :as marytts :refer [mary-interface audio-player]]))
 
-(defn initial []
+(defn core-initialize []
   (hypergraph/initialize)
   (marytts/initialize))
 
 (defn input-repl []
-  (do
     (print "you: ")
     (flush)
-    (read-line)))
+    (read-line))
 
-(defn output-repl [out]
-  (do
-  (print(str "eliza: " ))
-    (let [alGen (new DefaultALGenerator @graph (HGQuery$hg/type HGPlainLink) (HGQuery$hg/type String) false true false)
-          trav (new HGBreadthFirstTraversal (.getHandle @graph out) alGen)]
-      (print out)
-      (loop []
-        (when (= true (.hasNext trav))
-          (print (str (.get @graph (.getSecond (.next trav)))))
-          (recur)))
-    (marytts/play-audio out))
-  (print "\n")))
+(defn output-repl [input]
+  ((print "eliza: " )
+   (hypergraph/hg-bft input String)
+  ;(let [output (hypergraph/hg-bft "Hello" String)]
+  ;  (marytts/play-audio (clojure.string/join output))
+    (print "\n")))
 
-
-(defn hello-world []
-  ;TODO: Not used at the moment
-  (HGQuery$hg/getAll @graph (HGQuery$hg/type String)))
-
-(defn repl []
-  (loop []
+(defn repl [handle]
     (let [input (input-repl)]
-    (when (not= input "exit")
-        (output-repl "Hello")
-        (recur)))))
+    (while (not= input "exit")
+        (output-repl handle))))
 
-(defn add-items [vect]
-  "Adds items to graph and return a list of respective handles"
-  (map #(.add @graph %) vect))
-
-(defn add-links [vect item1 item2]
-  (do
-  (.add @graph (new HGPlainLink (into-array HGHandle [item1 item2])))
-  (let [index (.indexOf vect item2)]
-    ;If the index of the second element is less than size - 1
-    (if (< index (- (count vect) 1))
-      (add-links vect item2 (nth vect (+ index 1)))))))
-
-(defn temp-add []
-  "Adds few strings to hypergraphdb"
-    (let [items ["Hello" "," " World" "!"]
-          vect (add-items items)]
-      (add-links vect (nth vect 0) (nth vect 1))))
-      ;(doseq [link (HGQuery$hg/getAll @graph (HGQuery$hg/type HGPlainLink))]
-      ;(println link))))
-
-(defn temp-remove []
-  "Removes a temp strings from hypergraphdb"
-  (let [remove-list (HGQuery$hg/findAll @graph (HGQuery$hg/type String))]
-    (doseq [item remove-list]
-      (do
-        ;(println (.get graph item))
-        (.remove @graph item)))))
-  
 (defn -main [& args] 
-          ;(mary-set-voice mary)
-          (initial)
-          (temp-add)
-          (repl)
-          (temp-remove)
-          (.close @graph))
+     ((core-initialize)
+      ;(hypergraph/hg-temp-add)
+      (let [vect (hypergraph/hg-add-items ["Hello"])] 
+        (repl (first vect)))
+      (hypergraph/hg-temp-remove)
+      (hypergraph/hg-close)))
--- src/eliza/hypergraphdb.clj
+++ src/eliza/hypergraphdb.clj
@@ -3,9 +3,74 @@
   (:import [org.hypergraphdb HGEnvironment HyperGraph HGHandle HGQuery$hg HGPlainLink])
   (:import [org.hypergraphdb.algorithms DefaultALGenerator HGTraversal HGBreadthFirstTraversal]))
 
-;Should not be hardcoded in
+;TODO: should not be hardcoded in
 (def location "./db")
 (def graph (atom nil))
 
 (defn initialize []
   (reset! graph (HGEnvironment/get location)))
+
+(defn hg-close []
+  (.close @graph))
+
+; Input:    type 
+; Output    <ItemType> vector
+; Purpose:  get all nodes of a specific type 
+(defn hg-get-all [get-type]
+  (HGQuery$hg/getAll @graph (HGQuery$hg/type get-type)))
+
+; Input:    type 
+; Output    <HGHandle> vector
+; Purpose:  get all the handles of nodes of a specific type
+(defn hg-find-all [get-type]
+  (HGQuery$hg/findAll @graph (HGQuery$hg/type get-type)))
+
+; Input:    collection of items
+; Output:   <HGHandle> vector for corresponding items
+; Purpose:  add vector of items into graph
+(defn hg-add-items [coll]
+  (map #(.add @graph %) coll))
+
+; Input:    two <HGHandle> items
+; Output:   <HGHandle> link
+; Purpose:  establish a HGPlainLink between two items
+(defn hg-add-link [item1 item2]
+  (.add @graph (new HGPlainLink (into-array HGHandle [item1 item2]))))
+
+; Input:    vector of item HGHandles
+; Output:   vector of link HGHandles
+; Purpose:  establish a HGPlainLink between every
+;           item in the vector
+(defn hg-add-links [coll]
+  (let [parted-coll (partition 2 1 coll)]
+    (map #(hg-add-link (first %) (second %)) parted-coll)))
+
+; Input:    vector of item HGHandles
+; Purpose:  remove all referenced items
+(defn hg-remove-items [coll]
+  (doseq [item coll]
+    (.remove @graph item)))
+
+; Purpose:  add an array of strings to the hypergraph
+; TODO:     remove this in the future
+(defn hg-temp-add []
+    (let [items ["Hello" "," " World" "!"]
+          vect (hg-add-items items)]
+      (hg-add-links vect)))
+
+; Purpose:  remove all string nodes from hypergraph
+; TODO:     remove this in the future
+(defn hg-temp-remove []
+  (hg-remove-items (hg-find-all String)))
+
+; Input:    HGHandle of start node, type of nodes in traversal
+; Output:   <HGHandle> vector
+; Purpose:  Breadth-First-Traversal given a head node
+(defn hg-bft [node-handle get-type]
+  (let [alGen (new DefaultALGenerator @graph (HGQuery$hg/type HGPlainLink) (HGQuery$hg/type get-type) false true false)
+        trav (new HGBreadthFirstTraversal node-handle alGen)]
+    (while (= true (.hasNext trav))
+      (print (.getSecond (.next trav))))))
+
+(defn hg-temp-get [in]
+  (println (.getHandle @graph in)))
